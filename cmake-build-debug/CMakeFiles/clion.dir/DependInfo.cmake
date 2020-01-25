26a6e536b102930d8d866dccd206b4ed67af4960
Nkumar <niranjankumar213@gmail.com>
2020-01-25 10:48:23 +0530

    Initial Commit: Basic Structure C Program Project

---
+++ cmake-build-debug/CMakeFiles/clion.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/NHCE/clion/main.c" "E:/NHCE/clion/cmake-build-debug/CMakeFiles/clion.dir/main.c.obj"
+  "E:/NHCE/clion/new.c" "E:/NHCE/clion/cmake-build-debug/CMakeFiles/clion.dir/new.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ new.c
@@ -0,0 +1,11 @@
+//
+// Created by Nkumar on 1/25/2020.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World C!\n");
+    printf("Hello, World Second C!\n");
+    return 0;
+}
